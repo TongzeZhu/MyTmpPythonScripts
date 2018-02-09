e2be3f32a79557c7d050882005d30c5f2421e8e0
TongzeZhu <tongze.zhu@westwell-lab.com>
2018-02-09 14:52:29 +0800

    add a script for quick unix timestamp to time

---
+++ whenis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- encoding  utf-8 -*-
+from datetime import datetime as dt, timedelta
+import sys
+
+# convert unix timestamp(with microseconds) into readable time
+if len(sys.argv) >= 2:
+    atime = sys.argv[1]
+    atime_ms = 0
+    if not atime.isdecimal():
+        exit(0)
+    if len(atime) > 10:
+        atime_ms = int(atime[10:])
+        atime = atime[0:10]
+    atime = int(atime)
+    adate = dt.utcfromtimestamp(atime)
+    adate += timedelta(microseconds=atime_ms*1000)
+    print(adate)
