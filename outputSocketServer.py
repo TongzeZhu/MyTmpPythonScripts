5206b792c0be61b9a93382ca13e86004767b88f4
tongzezhu <tongze.zhu@westwell-lab.com>
2018-05-09 18:54:32 +0800

    server now send file data

--- outputSocketServer.py
+++ outputSocketServer.py
@@ -1,13 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- encoding utf8 -*-
 
-import SocketServer
+import socketserver
+import fileinput
 
-class MyTCPHandle(SocketServer.BaseRequestHandler):
+g_peers = []
+g_datafile = '/home/tongze/testspace/plcdata.txt'
+
+class MyTCPHandle(socketserver.BaseRequestHandler):
     def handle(self):
-        self.data = self.request.recv(4096).strip()
-        print "get one"
-        print self.data
+        global g_peers
+        if self.client_address[0] in g_peers:
+            self.request.sendall('one night, and no more!'.encode())
+            print('no more play')
+        else:
+            g_peers.append(self.client_address[0])
+            global g_datafile
+            for line in fileinput.input(g_datafile):
+                linelen = len(line)
+                if linelen == 0:
+                    continue
+                data = bytearray(2)
+                data[0] = int(linelen / 256)
+                data[1] = linelen % 256
+                self.request.sendall(data)
+                self.request.sendall(line.encode())
+            
+        
 
 if __name__ == "__main__":
-    server = SocketServer.TCPServer(("127.0.0.1", 9000), MyTCPHandle)
+    server = socketserver.TCPServer(("192.168.101.184", 9002), MyTCPHandle)
     server.serve_forever()
