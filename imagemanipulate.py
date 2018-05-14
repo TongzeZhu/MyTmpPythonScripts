5d1294136ffb25bf274a57057e235dcdf3d7bd4e
tongzezhu <tongze.zhu@westwell-lab.com>
2018-05-14 11:10:55 +0800

    add image cut function

--- imagemanipulate.py
+++ imagemanipulate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- encoding utf8 -*-
 
 from scipy import misc
@@ -6,12 +6,23 @@ import numpy
 
 
 def combineImage():
-    a = misc.imread("/home/tongze/test.jpg")
-    print(a.shape)
+    a = misc.imread("/home/tongze/testspace/testCombine.jpg")
+    # print(a.shape)
     c = a[:, :int(a.shape[1]/2), :].copy()
     for i in 0,4:
         # there are two braces after hstack
         c = numpy.hstack((c,a[:, int(a.shape[1]/2-100):512, :]))
-    misc.imsave('test.jpg', c)
+    savePath = '/home/tongze/testspace/combineImage.jpg'
+    misc.imsave(savePath, c)
+    print('New combined image saved to', savePath)
+
+def cutMidOfImage():
+    a = misc.imread("/home/tongze/testspace/testCut.jpg") 
+    # print(a.shape)
+    c = a[200:1300, 200:1200, :].copy()
+    savePath = '/home/tongze/testspace/midImage.jpg'
+    misc.imsave(savePath, c)
+    print('New cut image saved to', savePath)
 
 combineImage()
+cutMidOfImage()
