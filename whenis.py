03c6149e43cfe261008c2866c05b30280809c995
tongzezhu <tongze.zhu@westwell-lab.com>
2018-05-22 12:56:30 +0800

    make whenis tells current time's unix_timestamp when args is not number

--- whenis.py
+++ whenis.py
@@ -8,6 +8,7 @@ if len(sys.argv) >= 2:
     atime = sys.argv[1]
     atime_ms = 0
     if not atime.isdecimal():
+        print(dt.now().timestamp())
         exit(0)
     if len(atime) > 10:
         atime_ms = int(atime[10:])
